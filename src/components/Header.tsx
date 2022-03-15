c5d38653c3867c05ebd99ca50b9b20598df09b6c
Evyatar Haim <evyataros1@gmail.com>
2022-03-15 13:48:22 +0200

    building the Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,35 @@
-import { Box } from "@chakra-ui/react";
+import { Flex, Heading, HStack, Icon, Link } from "@chakra-ui/react";
 import React from "react";
-
+import { FiArrowUpRight } from "react-icons/fi";
 const Header = () => {
-  return <Box></Box>;
+  return (
+    <Flex
+      px="200px"
+      py="20px"
+      width="full"
+      bg="blue.900"
+      alignItems="flex-end"
+      justifyContent="space-between">
+      <Flex alignItems="flex-end">
+        <Heading
+          color="whiteAlpha.900"
+          mr="60px"
+          fontSize={20}
+          letterSpacing="1.5px">
+          My Logo
+        </Heading>
+        <HStack color="whiteAlpha.700" spacing="40px">
+          <Link>Home</Link>
+          <Link>Service</Link>
+          <Link>About Us</Link>
+        </HStack>
+      </Flex>
+      <Link color="whiteAlpha.800">
+        Get in touch
+        <Icon as={FiArrowUpRight} ml="10px" h={5} w={5} />
+      </Link>
+    </Flex>
+  );
 };
 
 export default Header;
--- src/pages/landing/sections/Inro.tsx
+++ src/pages/landing/sections/Inro.tsx
@@ -1,8 +1,31 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Heading, Icon, Link, Text } from "@chakra-ui/react";
 import React from "react";
+import { FiArrowUpRight } from "react-icons/fi";
 
 const Inro = () => {
-  return <Box></Box>;
+  return (
+    <Box w="full" bg="blue.900" px="200px" pt="60px">
+      <Flex justifyContent="space-between">
+        <Heading> חיים נעימים במדבר </Heading>
+        <Box maxWidth="300px">
+          <Text color="whiteAlpha.700" pb="20px">
+            מזמינים אתכם לחוויה קולינרית נפלאה: אוכל של בית, טעים ובריא, שמתבשל
+            אצלנו במטבח עם תבלין האהבה. הארוחות מוגשות בטברנה הקסומה שלנו
+            המשקיפה אל הנוף המדברי והרי אדום. התפריט מאוד מגוון, ובהזמנה מראש
+            אתם בוחרים מה תרצו לאכול: *ארוחת דגים *ארוחה בשרית *ארוחה
+            צמחונית/טבעונית ב'חיים נעימים במדבר' אנחנו מאוד אוהבים ליצור לכם
+            אווירה חמימה ותחושה של בית. המקום מתאים למשפחות, זוגות וקבוצות של עד
+            30 אנשים. *הזמנות מראש בלבד* מחכים לכם באהבה, שמוליק ונעמה נעמה
+            050-6325428 שמוליק 050-5408359
+          </Text>
+          <Link color="whiteAlpha.800">
+            Explore more
+            <Icon as={FiArrowUpRight} ml="10px" h={5} w={5} />
+          </Link>
+        </Box>
+      </Flex>
+    </Box>
+  );
 };
 
 export default Inro;
